525a23a4e652f5ae35555a0411a0d9c9e8a9aad1
Sebastian <Sebastiansal24@gmail.com>
2023-03-24 11:23:24 -0500

    Pausa del proyecto calculadora con poo y nuevo archivo

--- Calculadora_poo.py
+++ Calculadora_poo.py
@@ -1,31 +1,30 @@
 from tkinter import *
-#Raiz
-Root=Tk()
-frame=Frame(Root)
-frame.pack()
-Root.title("Calculadora")
-Numeropantalla=StringVar()
-Pulsado=""
-Pantalla=Entry(frame,textvariable=Numeropantalla)
-Pantalla.config(justify='right',bg="black",fg="white")
-Pantalla.grid(row=0,column=1,columnspan=4)
-
-
-#Funciones botones
-
-def pulsasiones(num):
-       global Pulsado
-       if Pulsado!="":
-           Numeropantalla.set(num)
-           Pulsado=""
-       else:
-           Numeropantalla.set(Numeropantalla.get() + str(num))
-     
-Uno=Button(frame,text=1,bg="Salmon",command=lambda:pulsasiones(1))
-Uno.grid(row=2,column=1)
-     
+class Calculadora():
+    def __init__(self,ventana):
+        self.ventana=ventana
+        self.ventana.title("Calculadora Poo")
+        self.frame1=Frame(ventana_Principal)
+        self.frame1.grid(row=1,column=1)
+        self.Numeropantalla=StringVar()
+        self.Operaciones=""
+        self.Pantalla=Entry(self.frame1,textvariable=self.Numeropantalla)
+        self.Pantalla.grid(row=0,column=1,columnspan=4,padx=10,pady=10)
+        self.Pantalla.config(width=27,bg="Black",fg="white")
+        Uno=Button(text=1,fg="red",bg="Black",foreground="white")
+        Uno.grid(row=2,column=0)
+        return
+    
 
 
 
 
-Root.mainloop()
+    
+    def Creacion_Botones():
+        pass
+
+
+
+
+ventana_Principal=Tk()
+Calculadora=Calculadora(ventana_Principal)
+ventana_Principal.mainloop()
